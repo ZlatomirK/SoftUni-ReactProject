4400abc09cfa80ba12c174c11086b51135a3d81e
ZlatomirK <zlatomirkolev090@gmail.com>
2023-12-03 22:10:30 +0200

    added login form to login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,39 @@
 const Login = () => {
   return (
     <main>
-      <h2>Login Page</h2>
+      <section id="login-page">
+        <div className="loginSection">
+          <form method="POST" className="loginForm">
+            <h2>Login</h2>
+            <ul className="noBullet">
+              <li>
+                <label htmlFor="email">Email:</label>
+                <input
+                  type="text"
+                  className="inputFields"
+                  id="email"
+                  name="email"
+                  placeholder="peter@abv.bg"
+                />
+              </li>
+              <li>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  className="inputFields"
+                  id="password"
+                  name="password"
+                  placeholder="*******"
+                />
+              </li>
+
+              <li id="center-btn">
+                <button id="login-btn">Login</button>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </section>
     </main>
   );
 };
